02edb302239c5eb472daf9a05a17270e01f4fec9
shauuebbit <58289901+shauuebbit@users.noreply.github.com>
2020-05-25 19:33:43 +0900

    Update dijkstra.cpp

--- library/graph/dijkstra.cpp
+++ library/graph/dijkstra.cpp
@@ -2,24 +2,25 @@
 #include<vector>
 
 template<typename T>
-std::vector<T> dijkstra(int src, const std::vector<std::vector<T>>& graph){
-  std::vector<T> distance(graph.size(), -1);
+std::vector<T> dijkstra(int src, const std::vector<std::vector<std::pair<T, int>>>& graph){
+  const T INF = 10000000000000000ll;
+  std::vector<T> distance(graph.size(), INF);
   std::priority_queue<std::pair<T, int>, std::vector<std::pair<T, int>>, std::greater<std::pair<T, int>>> pq;
 
   distance[src] = 0;
-  pq.push(std::make_pair(0, src));
+  pq.push({0, src});
 
-  while(!pq.size()){
+  while(pq.size()){
     auto [cost, node] = pq.top();
     pq.pop();
 
-    if(distance[node] >= 0 && distance[node] < cost) continue;
+    if(distance[node] < cost) continue;
 
     for(auto& [edge_cost, next] : graph[node]){
-      if(distance[next] >= 0 && distance[node] <= cost + edge_cost) continue;
+      if(distance[next] <= cost + edge_cost) continue;
 
       distance[next] = cost + edge_cost;
-      pq.push(std::make_pair(distance[next], next));
+      pq.push({distance[next], next});
     }
   }
 
